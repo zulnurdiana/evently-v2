63077a3e6226afbaba6317573b1651654ff7c9fd
Zulfikar Nurdiana <66067543+zulnurdiana@users.noreply.github.com>
2024-03-12 13:41:20 +0700

    first init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <html>
+      <body>
+        <h1 className="text-4xl">Evently</h1>
+        <Button variant={"outline"} className="px-10">
+          Click Me
+        </Button>
+      </body>
+    </html>
+  );
+}
