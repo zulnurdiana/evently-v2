64fe0adea13ef0be3bab67615998b27046af2b81
Zulfikar Nurdiana <66067543+zulnurdiana@users.noreply.github.com>
2024-03-12 13:59:21 +0700

    feat: header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,13 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <html>
-      <body>
-        <h1 className="text-4xl">Evently</h1>
-        <Button variant={"outline"} className="px-10">
-          Click Me
-        </Button>
-      </body>
-    </html>
+    <div>
+      <h2>Halaman Utama</h2>
+    </div>
   );
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href={"/"}>
+          <Image
+            src={"/assets/images/logo.svg"}
+            width={128}
+            height={32}
+            alt="Evently"
+          />
+        </Link>
+        <div className="flex justify-end w-32 gap-3"></div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
