102d1b77cd5035d27cfd8fbfc0fb37c5e2f41d49
Zulfikar Nurdiana <66067543+zulnurdiana@users.noreply.github.com>
2024-03-12 22:06:04 +0700

    feat: footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex md:flex-row gap-4 p-5 text-center flex-col">
+        <Image
+          src="/assets/images/logo.svg"
+          width={128}
+          height={38}
+          alt="logo"
+        />
+        <p>Evently. 2024 Alright Reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
