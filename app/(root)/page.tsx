fe24b97391cbc796dfc7c97b9a501cd7ef787a0a
Zulfikar Nurdiana <66067543+zulnurdiana@users.noreply.github.com>
2024-03-31 13:37:02 +0700

    feat: category filter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
 import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
@@ -54,6 +55,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
         </h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
+          <CategoryFilter />
         </div>
 
         <Collection
